dc32efc895ecac2ad2edd0d7ca6c3bd8afd4cbad
ivan <ivan@LatitudeE6520.informatik.uni-leipzig.de>
2017-02-23 16:39:23 +0100

    allow all hosts

--- bigknob/bigknob/settings.py
+++ bigknob/bigknob/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '83e84$)nt*9jy!azb6ba&4w2kt=y$_m(tkh&pn_og+70dbpcsj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
